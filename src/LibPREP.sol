0ea066877c3bffc7d11f31e32cbe0a909bb83d76
Vectorized <webby1111@hotmail.com>
2025-02-28 10:27:44 +0000

    Optimize

--- src/LibPREP.sol
+++ src/LibPREP.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.23;
 
 import {EfficientHashLib} from "solady/utils/EfficientHashLib.sol";
 import {LibBit} from "solady/utils/LibBit.sol";
-import {LibRLP} from "solady/utils/LibRLP.sol";
 import {ECDSA} from "solady/utils/ECDSA.sol";
 import {LibBytes} from "solady/utils/LibBytes.sol";
 import {LibEIP7702} from "solady/accounts/LibEIP7702.sol";
@@ -12,8 +11,6 @@ import {LibEIP7702} from "solady/accounts/LibEIP7702.sol";
 /// @notice A library to encapsulate the PREP (Provably Rootless EIP-7702 Proxy) workflow.
 /// See: https://blog.biconomy.io/prep-deep-dive/
 library LibPREP {
-    using LibRLP for LibRLP.List;
-
     /// @dev Validates if `digest` and `saltAndDelegation` results in `target`.
     /// Returns a non-zero `r` for the PREP signature, if valid.
     /// Otherwise returns 0.
@@ -31,7 +28,13 @@ library LibPREP {
     /// @dev Returns if `r` and `delegation` results in `target`.
     function isValid(address target, uint160 r, address delegation) internal view returns (bool) {
         bytes32 s = EfficientHashLib.hash(r);
-        bytes32 h = keccak256(abi.encodePacked(hex"05", LibRLP.p(0).p(delegation).p(0).encode()));
+        bytes32 h; // `keccak256(abi.encodePacked(hex"05", LibRLP.p(0).p(delegation).p(0).encode()))`.
+        assembly ("memory-safe") {
+            mstore(0x20, 0x80)
+            mstore(0x1f, delegation)
+            mstore(0x0b, 0x05d78094)
+            h := keccak256(0x27, 0x19)
+        }
         return ECDSA.tryRecover(h, 27, bytes32(uint256(r)), s) == target;
     }
 
