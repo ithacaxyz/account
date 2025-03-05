a85a6fb60339363dedafce476794c8378822ee19
Vectorized <webby1111@hotmail.com>
2025-03-05 20:26:51 +0000

    Add more test

--- test/GuardedExecutor.t.sol
+++ test/GuardedExecutor.t.sol
@@ -6,6 +6,9 @@ import "./Base.t.sol";
 import {GuardedExecutor} from "../src/Delegation.sol";
 
 contract GuardedExecutorTest is BaseTest {
+    mapping(uint256 => mapping(address => uint256)) expectedSpents;
+    mapping(uint256 => mapping(address => bool)) hasApproval;
+
     function setUp() public virtual override {
         super.setUp();
     }
@@ -114,15 +117,110 @@ contract GuardedExecutorTest is BaseTest {
         }
     }
 
+    function testSpends(bytes32) public {
+        EntryPoint.UserOp memory u;
+        DelegatedEOA memory d = _randomEIP7702DelegatedEOA();
+
+        u.eoa = d.eoa;
+        u.combinedGas = 1000000;
+        u.nonce = ep.getNonce(u.eoa, 0);
+
+        PassKey memory k = _randomSecp256k1PassKey();
+
+        address[] memory tokens = new address[](_bound(_randomUniform(), 1, 5));
+        for (uint256 i; i < tokens.length; ++i) {
+            tokens[i] = _randomChance(32) ? address(0) : LibClone.clone(address(paymentToken));
+        }
+        LibSort.sort(tokens);
+        LibSort.uniquifySorted(tokens);
+        for (uint256 i; i < tokens.length; ++i) {
+            _mint(tokens[i], u.eoa, type(uint192).max);
+        }
+
+        // Authorize.
+        {
+            ERC7821.Call[] memory calls = new ERC7821.Call[](2 + tokens.length);
+            // Authorize the key.
+            calls[0].data = abi.encodeWithSelector(Delegation.authorize.selector, k.k);
+            // As it's not a superAdmin, we shall just make it able to execute anything for testing sake.
+            calls[1].data = abi.encodeWithSelector(
+                GuardedExecutor.setCanExecute.selector, k.keyHash, _ANY_TARGET, _ANY_FN_SEL, true
+            );
+            for (uint256 i; i < tokens.length; ++i) {
+                // Set some spend limit.
+                calls[2 + i].data = abi.encodeWithSelector(
+                    GuardedExecutor.setSpendLimit.selector,
+                    k.keyHash,
+                    tokens[i],
+                    GuardedExecutor.SpendPeriod.Day,
+                    1 ether
+                );
+            }
+
+            u.executionData = abi.encode(calls);
+            u.nonce = 0xc1d0 << 240;
+
+            u.signature = _eoaSig(d.privateKey, u);
+
+            assertEq(ep.execute(abi.encode(u)), 0);
+            assertEq(d.d.spendInfos(k.keyHash).length, tokens.length);
+        }
+
+        // Test spends.
+        {
+            u.nonce = 0;
+            if (_randomChance(2)) {
+                for (uint256 i; i < tokens.length; ++i) {
+                    if (tokens[i] != address(0) && _randomChance(2)) {
+                        vm.prank(u.eoa);
+                        MockPaymentToken(tokens[i]).approve(address(0xb0b), _randomUniform());
+                    }
+                }
+            }
+
+            ERC7821.Call[] memory calls = new ERC7821.Call[](_bound(_randomUniform(), 1, 16));
+            for (uint256 i; i < calls.length; ++i) {
+                address token = tokens[_randomUniform() % tokens.length];
+                uint256 amount = _bound(_randomUniform(), 0, 0.000001 ether);
+                if (token != address(0) && _randomChance(2)) {
+                    calls[i].target = token;
+                    calls[i].data = abi.encodeWithSignature(
+                        "approve(address,uint256)", address(0xb0b), _random()
+                    );
+                    hasApproval[0][token] = true;
+                    continue;
+                }
+                calls[i] = _transferCall2(token, address(0xb0b), amount);
+                expectedSpents[0][token] += amount;
+            }
+            u.executionData = abi.encode(calls);
+            u.signature = _sig(k, u);
+
+            assertEq(ep.execute(abi.encode(u)), 0);
+            GuardedExecutor.SpendInfo[] memory infos = d.d.spendInfos(k.keyHash);
+            for (uint256 i; i < infos.length; ++i) {
+                assertEq(infos[i].spent, expectedSpents[0][infos[i].token]);
+            }
+
+            if (_randomChance(2)) {
+                for (uint256 i; i < tokens.length; ++i) {
+                    if (hasApproval[0][tokens[i]]) {
+                        assertEq(MockPaymentToken(tokens[i]).allowance(u.eoa, address(0xb0b)), 0);
+                    }
+                }
+            }
+        }
+    }
+
     function testSpendERC20WithSecp256r1ViaEntryPoint() public {
         _testSpendWithPassKeyViaEntryPoint(
-            _randomSecp256r1PassKey(), address(new MockPaymentToken())
+            _randomSecp256r1PassKey(), LibClone.clone(address(paymentToken))
         );
     }
 
     function testSpendERC20WithSecp256k1ViaEntryPoint() public {
         _testSpendWithPassKeyViaEntryPoint(
-            _randomSecp256k1PassKey(), address(new MockPaymentToken())
+            _randomSecp256k1PassKey(), LibClone.clone(address(paymentToken))
         );
     }
 
@@ -145,11 +243,11 @@ contract GuardedExecutorTest is BaseTest {
         u.nonce = ep.getNonce(u.eoa, 0);
         u.paymentToken = address(paymentToken);
         u.paymentAmount = 1 ether;
-        u.paymentMaxAmount = type(uint128).max;
+        u.paymentMaxAmount = type(uint192).max;
 
         // Mint some tokens.
-        paymentToken.mint(u.eoa, type(uint128).max);
-        _mint(tokenToSpend, u.eoa, type(uint128).max);
+        paymentToken.mint(u.eoa, type(uint192).max);
+        _mint(tokenToSpend, u.eoa, type(uint192).max);
 
         // Authorize.
         {
@@ -185,7 +283,7 @@ contract GuardedExecutorTest is BaseTest {
             u.nonce = 0;
 
             ERC7821.Call[] memory calls = new ERC7821.Call[](1);
-            calls[0] = _transferCall(tokenToSpend, address(0xb0b), 0.6 ether);
+            calls[0] = _transferCall2(tokenToSpend, address(0xb0b), 0.6 ether);
             u.executionData = abi.encode(calls);
 
             (gExecute, u.combinedGas,) = _estimateGas(k, u);
@@ -211,7 +309,7 @@ contract GuardedExecutorTest is BaseTest {
             u.nonce++;
 
             ERC7821.Call[] memory calls = new ERC7821.Call[](1);
-            calls[0] = _transferCall(tokenToSpend, address(0xb0b), 0.4 ether);
+            calls[0] = _transferCall2(tokenToSpend, address(0xb0b), 0.4 ether);
             u.executionData = abi.encode(calls);
 
             (gExecute, u.combinedGas,) = _estimateGas(k, u);
@@ -250,7 +348,7 @@ contract GuardedExecutorTest is BaseTest {
             u.nonce++;
 
             ERC7821.Call[] memory calls = new ERC7821.Call[](1);
-            calls[0] = _transferCall(tokenToSpend, address(0xb0b), 0.5 ether);
+            calls[0] = _transferCall2(tokenToSpend, address(0xb0b), 0.5 ether);
             u.executionData = abi.encode(calls);
 
             (gExecute, u.combinedGas,) = _estimateGas(k, u);
@@ -260,6 +358,16 @@ contract GuardedExecutorTest is BaseTest {
             assertEq(ep.execute{gas: gExecute}(abi.encode(u)), 0);
             assertEq(_balanceOf(tokenToSpend, address(0xb0b)), 1.5 ether);
             assertEq(d.d.spendInfos(k.keyHash)[0].spent, 0.5 ether);
+        }
+    }
+
+    function _transferCall2(address token, address to, uint256 amount)
+        internal
+        returns (ERC7821.Call memory c)
+    {
+        c = _transferCall(token, to, amount);
+        if (token != address(0) && _randomChance(2)) {
+            c.data = abi.encodeWithSignature("anotherTransfer(address,uint256)", to, amount);
         }
     }
 }
