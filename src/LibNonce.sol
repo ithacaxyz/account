ee3aae66e1a9f96cb373b38ac717b42a08acfeb0
Vectorized <webby1111@hotmail.com>
2025-03-06 00:39:37 +0000

    Tidy

--- src/LibNonce.sol
+++ src/LibNonce.sol
@@ -23,40 +23,42 @@ library LibNonce {
     ////////////////////////////////////////////////////////////////////////
 
     /// @dev Return current nonce with sequence key.
-    function get(mapping(uint192 => LibStorage.Ref) storage map, uint192 seqKey)
+    function get(mapping(uint192 => LibStorage.Ref) storage seqMap, uint192 seqKey)
         internal
         view
         returns (uint256)
     {
-        return map[seqKey].value | (uint256(seqKey) << 64);
+        return seqMap[seqKey].value | (uint256(seqKey) << 64);
     }
 
     /// @dev Increments the sequence for the `seqKey` in nonce (i.e. upper 192 bits).
     /// This invalidates the nonces for the `seqKey`, up to (inclusive) `uint64(nonce)`.
-    function invalidate(mapping(uint192 => LibStorage.Ref) storage map, uint256 nonce) internal {
-        LibStorage.Ref storage $ = map[uint192(nonce >> 64)];
-        if (uint64(nonce) < $.value) revert NewSequenceMustBeLarger();
-        $.value = Math.rawAdd(Math.min(uint64(nonce), 2 ** 64 - 2), 1);
+    function invalidate(mapping(uint192 => LibStorage.Ref) storage seqMap, uint256 nonce)
+        internal
+    {
+        LibStorage.Ref storage s = seqMap[uint192(nonce >> 64)];
+        if (uint64(nonce) < s.value) revert NewSequenceMustBeLarger();
+        s.value = Math.rawAdd(Math.min(uint64(nonce), 2 ** 64 - 2), 1);
     }
 
     /// @dev Checks that the nonce matches the current sequence.
-    function check(mapping(uint192 => LibStorage.Ref) storage map, uint256 nonce)
+    function check(mapping(uint192 => LibStorage.Ref) storage seqMap, uint256 nonce)
         internal
         view
-        returns (LibStorage.Ref storage $, uint256 seq)
+        returns (LibStorage.Ref storage s, uint256 seq)
     {
-        $ = map[uint192(nonce >> 64)];
-        seq = $.value;
+        s = seqMap[uint192(nonce >> 64)];
+        seq = s.value;
         if (!LibBit.and(seq < type(uint64).max, seq == uint64(nonce))) revert InvalidNonce();
     }
 
     /// @dev Checks and increment the nonce.
-    function checkAndIncrement(mapping(uint192 => LibStorage.Ref) storage map, uint256 nonce)
+    function checkAndIncrement(mapping(uint192 => LibStorage.Ref) storage seqMap, uint256 nonce)
         internal
     {
-        (LibStorage.Ref storage $, uint256 seq) = check(map, nonce);
+        (LibStorage.Ref storage s, uint256 seq) = check(seqMap, nonce);
         unchecked {
-            $.value = seq + 1;
+            s.value = seq + 1;
         }
     }
 }
