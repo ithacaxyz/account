8bbb48609dd4e52c8af731b9550a556c25b64d4c
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-04-11 14:28:34 +0530

    feat: complete prototype of new compensate

--- test/utils/mocks/MockEntryPoint.sol
+++ test/utils/mocks/MockEntryPoint.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import {EntryPoint} from "../../../src/EntryPoint.sol";
 import {Brutalizer} from "../Brutalizer.sol";
-import {UserOp} from "../../../src/structs/Common.sol";
 
 /// @dev WARNING! This mock is strictly intended for testing purposes only.
 /// Do NOT copy anything here into production code unless you really know what you are doing.
@@ -12,7 +11,7 @@ contract MockEntryPoint is EntryPoint, Brutalizer {
 
     constructor() payable EntryPoint(msg.sender) {}
 
-    function computeDigest(UserOp calldata userOp) public view returns (bytes32) {
+    function computeDigest(EntryPoint.UserOp calldata userOp) public view returns (bytes32) {
         return _computeDigest(userOp);
     }
 
