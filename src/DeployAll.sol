0fbb48415a678380c32e32e11af938a2c97e2c6f
Vectorized <webby1111@hotmail.com>
2025-04-15 19:11:49 +0000

    Move to DeployAll

--- src/DeployAll.sol
+++ src/DeployAll.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.23;
 import {EIP7702Proxy} from "solady/accounts/EIP7702Proxy.sol";
 import "../src/Delegation.sol";
 import "../src/EntryPoint.sol";
+import "../src/AccountRegistry.sol";
 
 contract DeployAll {
     address public immutable entryPoint;
     address public immutable delegationImplementation;
     address public immutable delegationProxy;
+    address public immutable accountRegistry;
 
     constructor(address deployer) payable {
         entryPoint = address(new EntryPoint(deployer));
         delegationImplementation = address(new Delegation(address(entryPoint)));
         delegationProxy = address(new EIP7702Proxy(delegationImplementation, address(0)));
+        accountRegistry = address(new AccountRegistry());
     }
 }
