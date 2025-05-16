2c5a5b9aa72fd2fcfd122f9f69b722a3e9731281
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-05-16 17:46:23 +0530

    chore: libTStack tests & fixes

--- src/libraries/LibTStack.sol
+++ src/libraries/LibTStack.sol
@@ -10,60 +10,25 @@ library LibTStack {
         t.slot = tSlot;
     }
 
-    function top(TStack memory t) internal view returns (bytes32) {
+    function top(TStack memory t) internal view returns (bytes32 val) {
         uint256 tSlot = t.slot;
 
         assembly ("memory-safe") {
             let len := tload(tSlot)
             if iszero(len) {
-                mstore(0x00, 0x00)
-                return(0x00, 0x20)
+                mstore(0x00, 0xbc7ec779) // `EmptyStack()`
+                revert(0x1c, 0x04)
             }
 
-            mstore(0x00, tload(add(tSlot, len)))
-
-            return(0x00, 0x20)
+            val := tload(add(tSlot, len))
         }
     }
 
-    function get(TStack memory t, uint256 index) internal view returns (bytes32) {
+    function size(TStack memory t) internal view returns (uint256 len) {
         uint256 tSlot = t.slot;
 
         assembly ("memory-safe") {
-            let len := tload(tSlot)
-
-            if lt(index, len) {
-                mstore(0x00, tload(add(tSlot, add(index, 1))))
-                return(0x00, 0x20)
-            }
-
-            mstore(0x00, 0xb4120f14) // `OutOfBounds()`
-            revert(0x1c, 0x04)
-        }
-    }
-
-    function set(TStack memory t, uint256 index, bytes32 val) internal {
-        uint256 tSlot = t.slot;
-
-        assembly ("memory-safe") {
-            let len := tload(tSlot)
-
-            if lt(index, len) {
-                tstore(add(tSlot, add(index, 1)), val)
-                return(0x00, 0x00)
-            }
-
-            mstore(0x00, 0xb4120f14) // `OutOfBounds()`
-            revert(0x1c, 0x04)
-        }
-    }
-
-    function size(TStack memory t) internal view returns (uint256) {
-        uint256 tSlot = t.slot;
-
-        assembly ("memory-safe") {
-            mstore(0x00, tload(tSlot))
-            return(0x00, 0x20)
+            len := tload(tSlot)
         }
     }
 
@@ -71,18 +36,57 @@ library LibTStack {
         uint256 tSlot = t.slot;
 
         assembly ("memory-safe") {
-            let len := tload(tSlot)
+            let len := add(tload(tSlot), 1)
             tstore(add(tSlot, len), val)
-            tstore(tSlot, add(len, 1))
+            tstore(tSlot, len)
         }
     }
 
+    /// @dev Does NOT clean the value on top of the stack automatically.
     function pop(TStack memory t) internal {
         uint256 tSlot = t.slot;
 
         assembly ("memory-safe") {
             let len := tload(tSlot)
+            if iszero(len) {
+                mstore(0x00, 0xbc7ec779) // `EmptyStack()`
+                revert(0x1c, 0x04)
+            }
+
             tstore(tSlot, sub(len, 1))
         }
     }
+
+    // Functions for feature completeness, that we don't need in the account.(UNTESTED)
+    // function get(TStack memory t, uint256 index) internal view returns (bytes32) {
+    //     uint256 tSlot = t.slot;
+
+    //     assembly ("memory-safe") {
+    //         let len := tload(tSlot)
+
+    //         if lt(index, len) {
+    //             mstore(0x00, tload(add(tSlot, add(index, 1))))
+    //             return(0x00, 0x20)
+    //         }
+
+    //         mstore(0x00, 0xb4120f14) // `OutOfBounds()`
+    //         revert(0x1c, 0x04)
+    //     }
+    // }
+
+    // function set(TStack memory t, uint256 index, bytes32 val) internal {
+    //     uint256 tSlot = t.slot;
+
+    //     assembly ("memory-safe") {
+    //         let len := tload(tSlot)
+
+    //         if lt(index, len) {
+    //             tstore(add(tSlot, add(index, 1)), val)
+    //             return(0x00, 0x00)
+    //         }
+
+    //         mstore(0x00, 0xb4120f14) // `OutOfBounds()`
+    //         revert(0x1c, 0x04)
+    //     }
+    // }
 }
