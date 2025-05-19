0fac59cf94de1aedca91f064df5a0687afd34a65
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-05-19 21:49:10 +0530

    chore: rename basic tests

--- test/DeployAll.t.sol
+++ test/DeployAll.t.sol
@@ -9,18 +9,16 @@ contract LibPREPTest is SoladyTest {
     function testDeployAll() public {
         DeployAll deployAll = new DeployAll();
 
-        address entryPoint = deployAll.entryPoint();
+        address orchestrator = deployAll.orchestrator();
 
-        assertEq(
-            Delegation(payable(deployAll.delegationImplementation())).ENTRY_POINT(), entryPoint
-        );
+        assertEq(Account(payable(deployAll.accountImplementation())).ORCHESTRATOR(), orchestrator);
 
         address eoa = _randomUniqueHashedAddress();
 
-        vm.etch(eoa, abi.encodePacked(hex"ef0100", deployAll.delegationImplementation()));
-        assertEq(Delegation(payable(eoa)).ENTRY_POINT(), entryPoint);
+        vm.etch(eoa, abi.encodePacked(hex"ef0100", deployAll.accountImplementation()));
+        assertEq(Account(payable(eoa)).ORCHESTRATOR(), orchestrator);
 
-        vm.etch(eoa, abi.encodePacked(hex"ef0100", deployAll.delegationProxy()));
-        assertEq(Delegation(payable(eoa)).ENTRY_POINT(), entryPoint);
+        vm.etch(eoa, abi.encodePacked(hex"ef0100", deployAll.accountProxy()));
+        assertEq(Account(payable(eoa)).ORCHESTRATOR(), orchestrator);
     }
 }
--- test/utils/mocks/MockOrchestrator.sol
+++ test/utils/mocks/MockOrchestrator.sol
@@ -1,29 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {EntryPoint} from "../../../src/EntryPoint.sol";
+import {Orchestrator} from "../../../src/Orchestrator.sol";
 import {Brutalizer} from "../Brutalizer.sol";
 
 /// @dev WARNING! This mock is strictly intended for testing purposes only.
 /// Do NOT copy anything here into production code unless you really know what you are doing.
-contract MockEntryPoint is EntryPoint, Brutalizer {
+contract MockOrchestrator is Orchestrator, Brutalizer {
     error NoRevertEncountered();
 
-    constructor(address pauseAuthority) EntryPoint(pauseAuthority) {}
+    constructor(address pauseAuthority) Orchestrator(pauseAuthority) {}
 
     function computeDigest(PreOp calldata preOp) public view returns (bytes32) {
         return _computeDigest(preOp);
     }
 
-    function computeDigest(UserOp calldata userOp) public view returns (bytes32) {
-        return _computeDigest(userOp);
+    function computeDigest(Intent calldata intent) public view returns (bytes32) {
+        return _computeDigest(intent);
     }
 
     /// @dev This function is provided for debugging purposes.
     /// This function bubbles up the full revert for the calls
     /// to `initializePREP` (if any) and `execute` on the eoa.
-    function simulateFailed(bytes calldata encodedUserOp) public payable virtual {
-        _execute(encodedUserOp, type(uint256).max, 1);
+    function simulateFailed(bytes calldata encodedIntent) public payable virtual {
+        _execute(encodedIntent, type(uint256).max, 1);
         revert NoRevertEncountered();
     }
 }
