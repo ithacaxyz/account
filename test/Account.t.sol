93ae234b40c600b6ba08d918c21dc6a4b17f6052
saurabh <saurabhyadav42399@gmail.com>
2025-06-15 04:22:58 +0530

    fix: add missing parameter to oc.execute() calls

--- test/Account.t.sol
+++ test/Account.t.sol
@@ -437,7 +437,7 @@ contract AccountTest is BaseTest {
         u1.signature = _sig(adminKey, u1);
 
         // Execute on chain 1 - should succeed
-        assertEq(oc.execute(abi.encode(u1)), 0, "Execution should succeed on chain 1");
+        assertEq(oc.execute(false, abi.encode(u1)), 0, "Execution should succeed on chain 1");
 
         // Verify keys were added on chain 1
         uint256 keysCount1 = IthacaAccount(eoaAddress).keyCount();
@@ -453,7 +453,7 @@ contract AccountTest is BaseTest {
         vm.etch(eoaAddress, abi.encodePacked(hex"ef0100", impl));
 
         // Execution should succeed due to multichain nonce in pre-calls
-        assertEq(oc.execute(abi.encode(baseIntent)), 0, "Should succeed due to multichain nonce");
+        assertEq(oc.execute(false, abi.encode(baseIntent)), 0, "Should succeed due to multichain nonce");
 
         // Verify keys were added on chain 137
         uint256 keysCount137 = IthacaAccount(eoaAddress).keyCount();
