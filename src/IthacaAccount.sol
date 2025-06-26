a2f5bc5b7ea205b1f1e82f81d4ee467713c5b2b1
jenpaff <jenpaff0@gmail.com>
2025-06-26 14:13:02 +0100

    chore: bump version to v0.3.3

--- src/IthacaAccount.sol
+++ src/IthacaAccount.sol
@@ -728,6 +728,6 @@ contract IthacaAccount is IIthacaAccount, EIP712, GuardedExecutor {
         returns (string memory name, string memory version)
     {
         name = "IthacaAccount";
-        version = "0.3.2";
+        version = "0.3.3";
     }
 }
