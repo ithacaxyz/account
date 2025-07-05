862f643f22678471f8f724f00bede119b7078a8b
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-07-05 22:37:06 +0400

    test: non invasive JIT pull flow

--- test/SubAccounts.t.sol
+++ test/SubAccounts.t.sol
@@ -238,4 +238,227 @@ contract SubAccountsTest is BaseTest {
         assertEq(oc.execute(false, abi.encode(intent)), 0);
         assertEq(counter.counter(), 2);
     }
+
+    function testJITPullFromMainWithERC20() public {
+        // Create an external key for the subAccount that points to the main account
+        IthacaAccount.Key memory externalKey = IthacaAccount.Key({
+            expiry: 0,
+            keyType: IthacaAccount.KeyType.External,
+            isSuperAdmin: true, // This external key is a super admin
+            publicKey: abi.encodePacked(mainAccount.eoa, bytes12(0))
+        });
+
+        // Authorize the external key on the subaccount
+        vm.prank(subAccount.eoa);
+        subAccount.d.authorize(externalKey);
+
+        // Main account approves the subAccount to use the main account's key
+        vm.prank(mainAccount.eoa);
+        mainAccount.d.setSubAccountApproval(mainKey.keyHash, subAccount.eoa, true);
+
+        // Create a non-super admin key for the main account with spend limit
+        PassKey memory spendLimitKey = _randomSecp256k1PassKey();
+        spendLimitKey.k.isSuperAdmin = false;
+        spendLimitKey.k.expiry = uint40(block.timestamp + 1 days);
+
+        // Main account authorizes the spend limit key
+        vm.prank(mainAccount.eoa);
+        mainAccount.d.authorize(spendLimitKey.k);
+
+        // Set 1000 token spend limit for ERC20 token
+        vm.prank(mainAccount.eoa);
+        mainAccount.d.setSpendLimit(
+            spendLimitKey.keyHash,
+            address(paymentToken),
+            GuardedExecutor.SpendPeriod.Day,
+            1000e18 // 1000 tokens with 18 decimals
+        );
+
+        // Main account approves the subAccount to use the spend limit key
+        vm.prank(mainAccount.eoa);
+        mainAccount.d.setSubAccountApproval(spendLimitKey.keyHash, subAccount.eoa, true);
+
+        // Grant the spend limit key permission to transfer tokens to subaccount
+        vm.prank(mainAccount.eoa);
+        mainAccount.d.setCanExecute(
+            spendLimitKey.keyHash, address(paymentToken), ERC20.transfer.selector, true
+        );
+
+        // Fund the main account with tokens
+        paymentToken.mint(mainAccount.eoa, 10000e18); // 10,000 tokens
+        assertEq(paymentToken.balanceOf(mainAccount.eoa), 10000e18);
+        assertEq(paymentToken.balanceOf(subAccount.eoa), 0);
+
+        // Create a P256 session key for the DApp
+        PassKey memory dappKey = _randomSecp256r1PassKey();
+        dappKey.k.isSuperAdmin = false;
+        dappKey.k.expiry = uint40(block.timestamp + 1 days);
+
+        // Create a DApp address to receive funds
+        address dappAddress = address(0xDABB);
+
+        // SubAccount atomically:
+        // 1. Authorizes the P256 session key
+        // 2. Grants permission to call execute on main account (needed for JIT pull)
+        // 3. Grants permission to transfer tokens
+        // 4. Set spend limit for the DApp key to allow token transfers
+        ERC7821.Call[] memory subAccountSetupCalls = new ERC7821.Call[](4);
+
+        subAccountSetupCalls[0] = ERC7821.Call({
+            to: subAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(subAccount.d.authorize.selector, dappKey.k)
+        });
+
+        subAccountSetupCalls[1] = ERC7821.Call({
+            to: subAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(
+                subAccount.d.setCanExecute.selector,
+                dappKey.keyHash,
+                mainAccount.eoa,
+                ERC7821.execute.selector,
+                true
+            )
+        });
+
+        subAccountSetupCalls[2] = ERC7821.Call({
+            to: subAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(
+                subAccount.d.setCanExecute.selector,
+                dappKey.keyHash,
+                address(paymentToken),
+                ERC20.transfer.selector,
+                true
+            )
+        });
+
+        subAccountSetupCalls[3] = ERC7821.Call({
+            to: subAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(
+                subAccount.d.setSpendLimit.selector,
+                dappKey.keyHash,
+                address(paymentToken),
+                GuardedExecutor.SpendPeriod.Day,
+                1000e18 // 1000 tokens
+            )
+        });
+
+        ICommon.Intent memory setupIntent;
+        setupIntent.eoa = subAccount.eoa;
+        setupIntent.combinedGas = 500_000;
+        setupIntent.executionData = abi.encode(subAccountSetupCalls);
+        setupIntent.nonce = 0;
+
+        // Sign with main account's key and wrap with external key hash
+        bytes32 externalKeyHash = _hash(externalKey);
+        bytes memory setupSig = _sig(mainKey, oc.computeDigest(setupIntent));
+        setupIntent.signature = abi.encodePacked(setupSig, externalKeyHash, uint8(0));
+
+        // Execute the setup
+        assertEq(oc.execute(false, abi.encode(setupIntent)), 0);
+
+        // Now the DApp wants to deposit 500 tokens somewhere
+        // The subaccount will JIT pull tokens from main account and then deposit
+
+        // First, create the inner call for main account to transfer tokens to subaccount
+        ERC7821.Call[] memory mainAccountCalls = new ERC7821.Call[](1);
+        mainAccountCalls[0] = ERC7821.Call({
+            to: address(paymentToken),
+            value: 0,
+            data: abi.encodeWithSelector(ERC20.transfer.selector, subAccount.eoa, 500e18)
+        });
+
+        // Encode the main account execute call with nonce and signature
+        // NOTE: In this case, for complete protection this requires 2 signatures.
+        // There are alternatives where the dapp session key is given direct transfer permission
+        // to the sub account. But this requires a session key that can check the calldata.
+        bytes memory mainAccountExecuteData = abi.encodePacked(
+            uint256(0), // nonce for main account
+            _sig(spendLimitKey, mainAccount.d.computeDigest(mainAccountCalls, 0))
+        );
+
+        // Create the subaccount's execution bundle
+        ERC7821.Call[] memory subAccountCalls = new ERC7821.Call[](2);
+
+        // First call: Pull funds from main account using the spend limit key
+        subAccountCalls[0] = ERC7821.Call({
+            to: mainAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(
+                ERC7821.execute.selector,
+                _ERC7821_BATCH_EXECUTION_MODE, // mode
+                abi.encode(mainAccountCalls, mainAccountExecuteData) // executionData
+            )
+        });
+
+        // Second call: Transfer tokens to DApp
+        subAccountCalls[1] = ERC7821.Call({
+            to: address(paymentToken),
+            value: 0,
+            data: abi.encodeWithSelector(ERC20.transfer.selector, dappAddress, 500e18)
+        });
+
+        ICommon.Intent memory depositIntent;
+        depositIntent.eoa = subAccount.eoa;
+        depositIntent.combinedGas = 500_000;
+        depositIntent.executionData = abi.encode(subAccountCalls);
+        depositIntent.nonce = 1;
+
+        // DApp signs with its P256 session key
+        depositIntent.signature = _sig(dappKey, depositIntent);
+
+        // Execute the JIT pull and deposit
+        assertEq(oc.execute(false, abi.encode(depositIntent)), 0);
+
+        // Verify the tokens moved correctly
+        assertEq(paymentToken.balanceOf(mainAccount.eoa), 9500e18); // 10000 - 500
+        assertEq(paymentToken.balanceOf(subAccount.eoa), 0);
+        assertEq(paymentToken.balanceOf(dappAddress), 500e18);
+
+        // Try to pull more than the spend limit (should fail)
+        mainAccountCalls[0] = ERC7821.Call({
+            to: address(paymentToken),
+            value: 0,
+            data: abi.encodeWithSelector(ERC20.transfer.selector, subAccount.eoa, 600e18) // Exceeds remaining daily limit
+        });
+
+        mainAccountExecuteData = abi.encodePacked(
+            uint256(1), // nonce for main account
+            _sig(spendLimitKey, mainAccount.d.computeDigest(mainAccountCalls, 1))
+        );
+
+        subAccountCalls[0] = ERC7821.Call({
+            to: mainAccount.eoa,
+            value: 0,
+            data: abi.encodeWithSelector(
+                ERC7821.execute.selector,
+                _ERC7821_BATCH_EXECUTION_MODE,
+                abi.encode(mainAccountCalls, mainAccountExecuteData)
+            )
+        });
+
+        subAccountCalls[1] = ERC7821.Call({
+            to: address(paymentToken),
+            value: 0,
+            data: abi.encodeWithSelector(ERC20.transfer.selector, dappAddress, 600e18)
+        });
+
+        depositIntent.executionData = abi.encode(subAccountCalls);
+        depositIntent.nonce = 2;
+        depositIntent.signature = _sig(dappKey, depositIntent);
+
+        // This should fail due to exceeding spend limit
+        assertEq(
+            oc.execute(false, abi.encode(depositIntent)),
+            bytes4(keccak256("ExceededSpendLimit(address)"))
+        );
+
+        // Verify no additional tokens were moved
+        assertEq(paymentToken.balanceOf(mainAccount.eoa), 9500e18);
+        assertEq(paymentToken.balanceOf(subAccount.eoa), 0);
+        assertEq(paymentToken.balanceOf(dappAddress), 500e18);
+    }
 }
