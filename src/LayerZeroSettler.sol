124e15f8593d5d85faff821455c812ae85b6862d
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-07-29 17:49:35 +0530

    chore: cleanup

--- src/LayerZeroSettler.sol
+++ src/LayerZeroSettler.sol
@@ -105,30 +105,14 @@ contract LayerZeroSettler is OApp, OAppOptionsType3, ISettler {
     /// @notice Allow contract to receive ETH from refunds
     receive() external payable {}
 
-    // @notice query price and assign jobs at the same time
-    // @param _dstEid - the destination endpoint identifier
-    // @param _sender - the source sending contract address. executors may apply price discrimination to senders
-    // @param _calldataSize - dynamic data size of message + caller params
-    // @param _options - optional parameters for extra service plugins, e.g. sending dust tokens at the destination chain
-    function assignJob(
-        uint32 _dstEid,
-        address _sender,
-        uint256 _calldataSize,
-        bytes calldata _options
-    ) external returns (uint256 price) {
+    // ========================================================
+    // ULN302 Executor Functions
+    // ========================================================
+    function assignJob(uint32, address, uint256, bytes calldata) external returns (uint256) {
         return 0;
     }
 
-    // @notice query the executor price for relaying the payload and its proof to the destination chain
-    // @param _dstEid - the destination endpoint identifier
-    // @param _sender - the source sending contract address. executors may apply price discrimination to senders
-    // @param _calldataSize - dynamic data size of message + caller params
-    // @param _options - optional parameters for extra service plugins, e.g. sending dust tokens at the destination chain
-    function getFee(uint32 _dstEid, address _sender, uint256 _calldataSize, bytes calldata _options)
-        external
-        view
-        returns (uint256 price)
-    {
+    function getFee(uint32, address, uint256, bytes calldata) external view returns (uint256) {
         return 0;
     }
 }
