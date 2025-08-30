32dab6f4100d59243bc134acec48cd24782d97d9
howy <132113803+howydev@users.noreply.github.com>
2025-08-30 12:23:52 -0400

    chore: undo blank line addns

--- test/Orchestrator.t.sol
+++ test/Orchestrator.t.sol
@@ -1478,12 +1478,10 @@ contract OrchestratorTest is BaseTest {
         emit Escrow.EscrowCreated(t.escrowIdBase);
         vm.prank(t.gasWallet);
         t.errs = oc.execute(t.encodedIntents);
-
         assertEq(uint256(bytes32(t.errs[0])), 0);
 
         // Verify funds are escrowed, not transferred yet
         vm.assertEq(t.usdcBase.balanceOf(address(t.escrowBase)), 600);
-
         vm.assertEq(t.usdcBase.balanceOf(t.relay), 0);
 
         // 4. Action on Arb
@@ -1495,7 +1493,6 @@ contract OrchestratorTest is BaseTest {
         t.encodedIntents[0] = abi.encode(t.baseIntent);
         vm.prank(t.gasWallet);
         t.errs = oc.execute(t.encodedIntents);
-
         assertEq(
             uint256(bytes32(t.errs[0])), uint256(bytes32(bytes4(keccak256("VerificationError()"))))
         );
@@ -1531,7 +1528,6 @@ contract OrchestratorTest is BaseTest {
         emit Escrow.EscrowCreated(t.escrowIdArb);
         vm.prank(t.gasWallet);
         t.errs = oc.execute(t.encodedIntents);
-
         assertEq(uint256(bytes32(t.errs[0])), 0);
         // Verify funds are escrowed, not transferred yet
         vm.assertEq(t.usdcArb.balanceOf(address(t.escrowArb)), 500);
