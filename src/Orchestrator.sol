050751a00a63e55f301f8be37f3fb6117db704fa
GitHub Action <action@github.com>
2025-09-26 16:29:12 +0000

    chore: bump contract versions due to bytecode changes - Contracts updated: IthacaAccount,Orchestrator,SimpleFunder

--- src/Orchestrator.sol
+++ src/Orchestrator.sol
@@ -899,7 +899,7 @@ contract Orchestrator is
         returns (string memory name, string memory version)
     {
         name = "Orchestrator";
-        version = "0.5.5";
+        version = "0.5.6";
     }
 
     ////////////////////////////////////////////////////////////////////////
