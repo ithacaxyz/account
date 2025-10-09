7142aafe2d1d2df5ffc60abe0d4dd8b44b628cd5
howy <132113803+howydev@users.noreply.github.com>
2025-10-09 01:28:24 -0400

    chore: remove paymaster nonce, rebench

--- test/utils/mocks/MockPayerWithSignatureOptimized.sol
+++ test/utils/mocks/MockPayerWithSignatureOptimized.sol
@@ -11,15 +11,10 @@ import {IOrchestrator} from "../../../src/interfaces/IOrchestrator.sol";
 
 contract MockPayerWithSignatureOptimized is Ownable {
     error InvalidSignature();
-    /// @dev The paymaster nonce has already been used.
-    error PaymasterNonceError();
 
     address public signer;
 
     address public immutable APPROVED_ORCHESTRATOR;
-
-    /// @dev Nonce management when acting as paymaster.
-    mapping(bytes32 => bool) public paymasterNonces;
 
     event Compensated(
         address indexed paymentToken,
@@ -67,12 +62,6 @@ contract MockPayerWithSignatureOptimized is Ownable {
         bytes calldata paymentSignature
     ) public virtual {
         if (msg.sender != APPROVED_ORCHESTRATOR) revert Unauthorized();
-
-        // Check and set nonce to prevent replay attacks
-        if (paymasterNonces[intentDigest]) {
-            revert PaymasterNonceError();
-        }
-        paymasterNonces[intentDigest] = true;
 
         bytes32 signatureDigest = computeSignatureDigest(intentDigest);
 
