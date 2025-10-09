34603e3d1e096597ab4859c3610500b5fac1d850
howydev <132113803+howydev@users.noreply.github.com>
2025-10-09 05:31:01 +0000

    chore: bump contract versions due to bytecode changes - Contracts updated: IthacaAccount,Orchestrator,SimpleFunder

--- src/SimpleFunder.sol
+++ src/SimpleFunder.sol
@@ -56,7 +56,7 @@ contract SimpleFunder is EIP712, Ownable, IFunder {
         returns (string memory name, string memory version)
     {
         name = "SimpleFunder";
-        version = "0.1.8";
+        version = "0.1.9";
     }
 
     ////////////////////////////////////////////////////////////////////////
