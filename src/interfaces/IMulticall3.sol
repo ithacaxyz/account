1fe51b3571881431ce11175d3ea5cfe3c26ba4c7
howy <132113803+howydev@users.noreply.github.com>
2025-10-20 22:20:01 -0400

    feat: add multicall to simulator (#402)
    * feat: add multicall to simulator
    
    * chore: add gas test
    
    * chore: review fixes
    
    * chore: fmt contracts and update gas snapshots

---
+++ src/interfaces/IMulticall3.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/// @notice Interface for Multicall3 contract
+interface IMulticall3 {
+    struct Call {
+        address target;
+        bytes callData;
+    }
+
+    struct Call3 {
+        address target;
+        bool allowFailure;
+        bytes callData;
+    }
+
+    struct Call3Value {
+        address target;
+        bool allowFailure;
+        uint256 value;
+        bytes callData;
+    }
+
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function aggregate(Call[] calldata calls)
+        external
+        payable
+        returns (uint256 blockNumber, bytes[] memory returnData);
+
+    function aggregate3(Call3[] calldata calls)
+        external
+        payable
+        returns (Result[] memory returnData);
+}
